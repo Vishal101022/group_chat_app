d4ce9bc8ebcb75e7896cc2b093c9086eec6f9f79
Vishal101022 <raivishal939@gmail.com>
2024-11-14 21:03:21 +0530

    Add business logic

---
+++ controllers/loginController.js
@@ -0,0 +1,26 @@
+const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        // check user exists or not
+        const user = await userModel.findOne({ where: { email } });
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        // compare password
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+        // generate token
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        return res.status(200).json({message: "Login successful", token});
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+}
