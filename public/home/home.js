7b30b563eb4bd0ec5c16ace98548aa178408508f
Vishal101022 <raivishal939@gmail.com>
2024-11-15 21:39:14 +0530

    Add fn to display messages on screen

--- public/home/home.js
+++ public/home/home.js
@@ -1,5 +1,11 @@
 const token = localStorage.getItem("token");
 
+// initialize the chat app
+window.addEventListener("DOMContentLoaded", async () => {
+    document.getElementById("send-button").addEventListener("click", sendMessage);
+    document.getElementById("logout-button").addEventListener("click", logout);
+    await fetchMessages();
+})
 async function sendMessage() {
     const input = document.getElementById("message-input");
     const message = input.value.trim();
@@ -17,5 +23,30 @@ async function sendMessage() {
         }
     }
 }
+async function fetchMessages() {
+    try {
+        const response = await axios.get("http://localhost:3000/api/messages", { headers: { Authorization: `${token}` } });
+        displayMessages(response.data.messages);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-document.getElementById("send-button").addEventListener("click", sendMessage);
+function displayMessages(messages) {
+    const chatArea = document.getElementById("chat-area");
+    chatArea.innerHTML = "";
+    messages.forEach((message) => {
+        const messageElement = document.createElement("div");
+        messageElement.classList.add("message");
+        messageElement.textContent = `${message.User.name}: ${message.message}`;
+        chatArea.appendChild(messageElement);
+    });
+}
+
+function logout() {
+    localStorage.removeItem("token");
+    window.location.href = "/public/login/login.html";
+}
+
+
+
