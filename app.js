148c0eee318cf8f4cb42d062a564c9c9592a0f6b
Vishal101022 <raivishal939@gmail.com>
2024-11-16 15:10:26 +0530

    :recycle: clean the file & import the socket connection

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const cors = require("cors");
 const db = require("./util/db");
 const dotenv = require("dotenv");
 dotenv.config();
-const { createServer } = require("http");
-const { Server } = require("socket.io");
 const port = process.env.PORT || 3000;
+const {socketConnection, server} = require("./util/socketIo");
 
 // routes
 const userRoutes = require("./routes/userRoutes");
@@ -22,8 +21,6 @@ const corsOptions = {
 };
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server, {cors: corsOptions,});
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use("/api", userRoutes);
@@ -41,19 +38,8 @@ async function testConnection() {
   }
 }
 testConnection();
-
-// socket connection
-io.on("connection", (socket) => {
-  console.log("A user connected");
-  socket.on("messageSent", (data) => {
-    console.log(data, socket.id);
-    io.emit("messageReceived", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
-});
+// initialize socket connection
+socketConnection();
 
 server.listen(port, () => {
   console.log("Server started on port 3000");
