aac235387de6e9c585495147285cd73f54d857ec
Vishal101022 <raivishal939@gmail.com>
2024-11-18 17:46:51 +0530

    Add socketMiddleware fn

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,18 +6,48 @@ dotenv.config();
 exports.authMiddleware = async (req, res, next) => {
   try {
     const token = req.header("Authorization");
-
+    if (!token) {
+      return res.status(401).json({ error: "Authorization token is missing" });
+    }
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-
     const userExits = await User.findByPk(payload.userId);
 
     if (!userExits) {
       return res.status(404).json({ error: "User not found" });
     }
 
-    req.user = userExits.id;
+    req.user = {
+      id: userExits.id,
+      name: userExits.name,
+    };
     next();
   } catch (err) {
     return res.status(500).json({ error: err.message });
+  }
+};
+
+exports.socketMiddleware = async (socket, next) => {
+  try {
+    const token = socket.handshake.headers.authorization; 
+
+    if (!token) {
+      return next(new Error("Authorization token is missing"));
+    }
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    const userExists = await User.findByPk(payload.userId);
+
+    if (!userExists) {
+      return next(new Error("User not found"));
+    }
+
+    socket.user = {
+      id: userExists.id,
+      name: userExists.name
+    }
+    next(); 
+  } catch (err) {
+    next(new Error("Authentication error")); // Pass error to Socket.io
   }
 };
